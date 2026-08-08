31b7af15f0116079f492509546d2617c75725c2d
Arthur <aadjamoglian@gmail.com>
2026-08-07 18:04:53 -0700

    Added some 401 responses for deficiencies

--- api.py
+++ api.py
@@ -9,13 +9,22 @@ app = Flask(__name__)
 def genearte_graph():
     data = request.get_json()
 
-    fig = px.scatter(x=data['x_data'], y=data['y_data'], title = data['graph_title'])
+    if "x_data" in data and "y_data" in data and "graph_title" in data:
+        if len(data["x_data"]) == len(data["x_data"]):
+            if len(data["x_data"]) > 0:
+                fig = px.scatter(x=data['x_data'], y=data['y_data'], title = data['graph_title'])
 
-    fig_json = fig.to_json()
+                fig_json = fig.to_json()
 
-    # return data, 200
-    return app.response_class(fig_json, mimetype="application/json")
+                # return data, 200
+                return app.response_class(fig_json, mimetype="application/json"), 200
+            else:
+                return "Bad Request: No points for data.", 401
+        else:
+            return "Bad Request: X and Y data length mismatch.", 401
 
+    else:
+        return "Bad Request: Missing x data, y data, or title.", 401
 
 if __name__ == '__main__':
     app.run(debug=True)
