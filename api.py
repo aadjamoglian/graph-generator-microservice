b62e72597792869beb03ef3c638d0ce5ed7033ca
Arthur <aadjamoglian@gmail.com>
2026-08-07 18:10:27 -0700

    Cleaned up and added some comments

--- api.py
+++ api.py
@@ -9,17 +9,22 @@ app = Flask(__name__)
 def genearte_graph():
     data = request.get_json()
 
+    # Check to make sure all properties were included
     if "x_data" in data and "y_data" in data and "graph_title" in data:
+
+        # To prevent plotly error
         if len(data["x_data"]) == len(data["x_data"]):
+
+            # Empty graph no point
             if len(data["x_data"]) > 0:
                 fig = px.scatter(x=data['x_data'], y=data['y_data'], title = data['graph_title'])
-
                 fig_json = fig.to_json()
 
-                # return data, 200
                 return app.response_class(fig_json, mimetype="application/json"), 200
+
             else:
                 return "Bad Request: No points for data.", 401
+                
         else:
             return "Bad Request: X and Y data length mismatch.", 401
 
